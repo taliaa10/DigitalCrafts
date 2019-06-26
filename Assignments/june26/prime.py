0a4d6a079928aaeb48089759f8d4aea75dd787a0
Taliaa <taliaa10@ymail.com>
2019-06-26 14:54:36 -0500

    assignments

--- Assignments/june26/prime.py
+++ Assignments/june26/prime.py
@@ -9,17 +9,3 @@ def prime():
         return True
 
 print(prime())
-
-
-# def test_prime():
-#     n = int(input("Check if prime number: "))
-#     if (n==1):
-#         return False
-#     # elif (n==2):
-#     #     return True;
-#     else:
-#         for i in range(2,n):
-#             if(n % i == 0):
-#                 return False
-#         return True             
-# print(test_prime())
