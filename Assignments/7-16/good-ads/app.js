c8b4a26c209fdd90a3557aac6c4d40b5c08525b0
Taliaa <taliaa10@ymail.com>
2019-07-16 16:15:34 -0500

    todo list with javascript

---
+++ Assignments/7-16/good-ads/app.js
@@ -0,0 +1,4 @@
+// get the adImage by id
+let adImage = document.getElementById("adImage")
+// set the src property to the new random ad image  
+adImage.src = getRandomAd()
---
+++ Assignments/7-16/to-do-list-js/tasks.js
@@ -0,0 +1,49 @@
+
+
+let taskNameTextbox = document.getElementById("task-item-textbox")
+let enterTaskButton = document.getElementById("enter-task-button")
+
+let pendingTasksListDiv = document.getElementById("pending-tasks-list-div")
+let completedTasksListDiv = document.getElementById("completed-tasks-list-div")
+
+enterTaskButton.addEventListener('click', function() {
+
+    // create div for list items
+    let listItemDiv = document.createElement("li")
+
+    let taskName = taskNameTextbox.value
+
+    let checkbox = document.createElement("input")
+    checkbox.setAttribute("type", "checkbox")
+    checkbox.setAttribute("class", "checkbox")
+    checkbox.addEventListener("click", function() {
+
+        completedTasksListDiv.appendChild(listItemDiv)
+        if (this.checked == false) {
+        pendingTasksListDiv.appendChild(listItemDiv)
+        }
+    })
+
+    let taskNameSpan = document.createElement("span")
+    taskNameSpan.innerHTML = taskName
+
+
+    // REMOVE TASK ITEM
+    let removeButton = document.createElement("button")
+    removeButton.setAttribute("id", "remove-task-button")
+    removeButton.innerHTML = "Remove"
+    removeButton.addEventListener("click", function() {
+
+        pendingTasksListDiv.removeChild(pendingTasksListDiv.childNodes[0])
+    })
+
+    listItemDiv.appendChild(checkbox)
+    listItemDiv.appendChild(taskNameSpan)
+    listItemDiv.appendChild(removeButton)
+
+    pendingTasksListDiv.appendChild(listItemDiv)
+
+})
+
+// let removeTaskButton = document.getElementById("remove-task-button")
+
